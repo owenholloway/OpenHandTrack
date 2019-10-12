9713b98ca39306d00f50e0a4258aedebf4148b92
Owen Holloway <owen.holloway101@gmail.com>
2019-10-12 12:56:51 +1100

    boxy plotty

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import alglib.processing as processing
 import alglib.two_filter as two_filter
 import alglib.colour_space as colour
 import alglib.convex_hull as convexhull
+import time
 from vision import init
 
 X_RESOLUTION = 1080
@@ -20,6 +21,8 @@ cap = cv2.VideoCapture("hand_test/hands_test_4.mov")
 
 while True:
 
+    start = time.time()
+
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -31,29 +34,33 @@ while True:
 
     contours = processing.filter_contours(contours)
 
-    preHist = processing.hsv_histogram(colour.hsv(frame))
+    #preHist = processing.hsv_histogram(colour.hsv(frame))
 
-    postHist = processing.hsv_histogram(colour.hsv(filtered_frame))
+    #postHist = processing.hsv_histogram(colour.hsv(filtered_frame))
+
+
 
     if len(contours) > 0:
 
         max_contour = max(contours, key=cv2.contourArea)
-        rect = cv2.minAreaRect(max_contour)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
+        #rect = cv2.minAreaRect(max_contour)
+        #box = cv2.boxPoints(rect)
+        #box = np.int0(box)
 
         points = convexhull.get_hull_points(max_contour)
         clusted_points = processing.contour_clustering(max_contour, points, 20)
-        convexhull.draw_hull_on_frame(frame_gpu, max_contour)
+        #convexhull.draw_hull_on_frame(frame_gpu, max_contour)
 
-        for i in range(0, len(clusted_points)):
-            cv2.circle(frame_gpu, (int(clusted_points[i][0][0][0]), int(clusted_points[i][0][0][1])), 4, (211, 0, 255), 2)
+        #for i in range(0, len(clusted_points)):
+            #cv2.circle(frame_gpu, (int(clusted_points[i][0][0][0]), int(clusted_points[i][0][0][1])), 4, (211, 0, 255), 2)
             #cv2.putText(frame_gpu, str(i), (int(clusted_points[i][0][0][0]), int(clusted_points[i][0][0][1])), cv2.FONT_HERSHEY_SIMPLEX, 1.0, 25)
 
         #cv2.drawContours(frame_gpu, [box], 0, (0, 0, 255), 1)
         #cv2.drawContours(frame_gpu, [max_contour], -1, (255, 0, 0), 1, 1)
 
-    cv2.imshow('Output Frame', cv2.resize(frame_gpu, None, fx=1, fy=1))
+    end = time.time()
+    print((end - start)*1000)
+    #cv2.imshow('Output Frame', cv2.resize(frame_gpu, None, fx=1, fy=1))
     #cv2.imshow('Post Frame', cv2.resize(filtered_frame, None, fx=1, fy=1))
     #cv2.imshow('Pre Hist', preHist)
     #cv2.imshow('Post Hist', postHist)
